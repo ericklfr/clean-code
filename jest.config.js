ff01233a22aae2140b87cd567d432152b9a060ba
Erick Leal França <erick_franca1@hotmail.com>
2020-10-03 13:06:27 -0300

    chore: First setup, added dependencies and libs

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  coverageDirectory: 'covarage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+};
